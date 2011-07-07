18520c41b124a1ba2eb4e3604905ca80370e931f
Erika Thea <erika.orangeandbronze.com>
2011-07-07 16:34:05 +0800

    schema: ozmness password-enabled(admin) Signed-off-by: Erika Thea <erika.orangeandbronze.com>

--- ozmness/grails-app/conf/BootStrap.groovy
+++ ozmness/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,31 @@
 class BootStrap {
-
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+	def springSecurityService
+	def init = { servletContext ->
+		def roles = [:]
+		def users = [:]
+		roles.admin = Role.findByAuthority("ROLE_ADMIN") ?: \
+			new Role(authority: "ROLE_ADMIN").save()
+		users.admin = User.findByUsername('admin') ?: \
+			new User(username: 'admin',
+			password: springSecurityService.encodePassword('admin'), \
+			enabled: true, \
+			accountExpired:false, \
+			accountLocked: false, \
+			passwordExpired: false).save()
+		UserRole.create( users.admin, roles.admin )
+		
+	roles.admin = Role.findByAuthority("ROLE_DEV") ?: \
+			new Role(authority: "ROLE_DEV").save()
+		users.admin = User.findByUsername('dev') ?: \
+			new User(username: 'dev',
+			password: springSecurityService.encodePassword('dev'), \
+			enabled: true, \
+			accountExpired:false, \
+			accountLocked: false, \
+			passwordExpired: false).save()
+		UserRole.create( users.dev, roles.dev )
+	}
+	
+	def destroy = {
+	}
 }
