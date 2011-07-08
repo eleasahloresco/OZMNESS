6ae8f4c3a077fcb8effb6fa7cad27ed54251a7b9
Mark Basmayor <mark.basmayor@gmail.com>
2011-07-08 15:41:27 +0800

    added tests for project, added fields to employee

--- grails-app/domain/com/onb/ozmness/Employee.groovy
+++ grails-app/domain/com/onb/ozmness/Employee.groovy
@@ -1,10 +1,31 @@
 package com.onb.ozmness
 
-class Employee extends User{
-    
-    static hasMany =[projects:Project]
+class Employee extends User {
+
+    def firstName
+    def lastName
+    static hasMany = [projects:Project]
     Employee mentor
     EmployeePosition position
+
     static constraints = {
     }
+
+    /*
+    @Override
+    int hashCode() {
+        // TODO
+        //"[$name][$type]".hashCode()
+        1
+    }
+    */
+
+    @Override
+    boolean equals (Object o) {
+        (o instanceof Employee) &&
+        (this.firstName  == o.firstName) &&
+        (this.lastName == o.lastName)
+        // TODO
+    }
+
 }
--- grails-app/domain/com/onb/ozmness/Project.groovy
+++ grails-app/domain/com/onb/ozmness/Project.groovy
@@ -2,9 +2,10 @@ package com.onb.ozmness
 
 class Project {
     
-    String name
+    def name
     Employee lead
     static hasMany = [technologies : Technology]
+
     static constraints = {
     }
 }
