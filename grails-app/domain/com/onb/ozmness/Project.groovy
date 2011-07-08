de3292b1f71456d61376f5c6a9a64a5a4ab111d0
Froilan Co <froilan.co@orangeandbronze.com>
2011-07-08 16:46:41 +0800

    updated Project domain
    Signed-off-by: Froilan Co <froilan.co@orangeandbronze.com>

--- grails-app/domain/com/onb/ozmness/Project.groovy
+++ grails-app/domain/com/onb/ozmness/Project.groovy
@@ -1,11 +1,13 @@
 package com.onb.ozmness
 
 class Project {
-    
-    def name
+
+    String name
     Employee lead
     static hasMany = [technologies : Technology]
 
     static constraints = {
+        lead nullable:false, blank:false
+        name nullable:false, blank:false
     }
 }
