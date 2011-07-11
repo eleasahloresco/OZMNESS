18431ce69658e1d4dd9e60e342c6375a5287876b
Eleasah <eleasah.loresco@orangeandbronze.com>
2011-07-11 16:20:32 +0800

    moved the test to RatingTests.groovy
    Signed-off-by: Eleasah <eleasah.loresco@orangeandbronze.com>

--- test/unit/com/onb/ozmness/RatingTests.groovy
+++ test/unit/com/onb/ozmness/RatingTests.groovy
@@ -3,15 +3,28 @@ package com.onb.ozmness
 import grails.test.*
 
 class RatingTests extends GrailsUnitTestCase {
+     def simpleDateFormat
+
     protected void setUp() {
         super.setUp()
+	mockDomain(Employee)
+	mockDomain(Technology)
+	mockDomain(Rating)
+	simpleDateFormat = new SimpleDateFormat("ddMMyyyy")
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-    void testSomething() {
-
+    void testRatingIfSaved() {
+	def rating = new Rating(value: 3,
+				comment: "Good Job",
+				dateOfCreation: simpleDateFormat.parse("07082011"),
+				creator: new Employee(),
+				rated: new Employee(),
+				technology: new Technology())
+	rating.save()
+	assertNotNull("If the rating is saved, there should be an ID stored in the database", rating.id)
     }
 }
