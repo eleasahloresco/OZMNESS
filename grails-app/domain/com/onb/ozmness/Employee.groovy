ac900ceb88d121b8baded238b83cb21cc11fa4ef
Mark Basmayor <mark.basmayor@gmail.com>
2011-07-14 13:58:23 +0800

    removed extraneous code

--- grails-app/domain/com/onb/ozmness/Employee.groovy
+++ grails-app/domain/com/onb/ozmness/Employee.groovy
@@ -7,26 +7,17 @@ class Employee extends User {
     Employee mentor
     EmployeePosition position
 
-    //static belongsTo = Project
     static hasMany = [projects: Project]
 
     static constraints = {
-	mentor		nullable: true, blank: true
-	position	nullable: false, blank: false
-	firstName	nullable: false, blank: false
-	lastName	nullable: false, blank: false
-    }
-
-    @Override
-    boolean equals (Object o) {
-        (o instanceof Employee) &&
-        (this.firstName  == o.firstName) &&
-        (this.lastName == o.lastName)
-        // TODO
+        mentor		nullable: true, blank: true
+        position	nullable: false, blank: false
+        firstName	nullable: false, blank: false
+        lastName	nullable: false, blank: false
     }
 
 	String toString(){
-		firstName + " " + lastName	
+		firstName + " " + lastName
 	}
 
 }
